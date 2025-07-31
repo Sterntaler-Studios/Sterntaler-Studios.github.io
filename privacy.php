b2753eda7037a3e9bd00415a7d336e18d761704e
Sterntaler-Studios <sterntalerstudios@gmail.com>
2025-07-31 09:02:49 -0500

    Add files via upload

---
+++ privacy.php
@@ -0,0 +1,23 @@
+<?php
+
+// Start the session
+session_start();
+
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+?>
+<!DOCTYPE html>
+<html lang="en">
+<?php include "siteElements/head.php"; ?>
+
+<body class="black">
+    <?php include "siteElements/content.php"; ?>
+<div style="margin: 0 auto;">
+    <iframe width="560" height="315" src="https://www.youtube.com/embed/fXZj4Wy58Pk?si=0TwQGa_s1Zb3P7pb" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+        </div>
+</body>
+
+</html>
+
+?>
